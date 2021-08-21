46c915e093e9e55a750c96e9a97f5a438255654c
Evan Mozumder <evanmozumder5@gmail.com>
2021-08-21 21:52:44 +0600

    first commit

---
+++ js/test.js
@@ -0,0 +1,52 @@
+// extra components and delivery cost class
+const optionsCost = document.getElementsByClassName("option-cost");
+
+// all components option button class
+const allOptions = document.getElementsByClassName("option-btn");
+
+// get all elemets using their corresponding ID
+function getElementUsingId(id) {
+	return document.getElementById(id);
+}
+/* function updateOptionPrice(optionId, price) {
+	document.getElementById(optionId).innerText = price;
+	updateTotalCost();
+} */
+
+// update price after choosing extra components
+function updateValue(id, value) {
+	getElementUsingId(id).innerText = value;
+}
+
+// calculating total price
+function updateTotalCost() {
+	let totalPrice = 1299;
+	for (const option of optionsCost) {
+		totalPrice += parseInt(option.innerText);
+	}
+	updateValue("total-price", totalPrice);
+	updateValue("total-price-coupon", totalPrice);
+	return totalPrice;
+}
+
+// update price of corresponding components after choosing extra components
+for (const option of allOptions) {
+	option.addEventListener("click", (event) => {
+		if (event.target.id.includes("memory")) {
+			updateValue("memory-cost", event.target.value);
+		} else if (event.target.id.includes("storage")) {
+			updateValue("storage-cost", event.target.value);
+		} else if (event.target.id.includes("delivery")) {
+			updateValue("delivery-cost", event.target.value);
+		}
+	});
+}
+
+// add event listener to coupon apply button to cut 20% price
+document.getElementById("coupon-btn").addEventListener("click", () => {
+	totalPrice = updateTotalCost();
+	if (getElementUsingId("coupon-code").value == "stevekaku") {
+		updateValue("total-price-coupon", totalPrice - totalPrice / 5);
+	}
+	getElementUsingId("coupon-code").value = "";
+});
